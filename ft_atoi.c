f7db3f888d3af9546d74695a4861e72d09c42083
rgomes-d <rgomes-d@student.42sp.org.br>
2025-07-16 11:06:54 -0300

    feat: função atoi adicionada.

--- ft_atoi.c
+++ ft_atoi.c
@@ -6,17 +6,30 @@
 /*   By: rgomes-d <rgomes-d@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/15 20:03:25 by rgomes-d          #+#    #+#             */
-/*   Updated: 2025/07/15 20:08:56 by rgomes-d         ###   ########.fr       */
+/*   Updated: 2025/07/16 10:56:07 by rgomes-d         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
-#include <limits.h>
 
-int		ft_atoi(const char *nptr)
+int	ft_atoi(const char *nptr)
 {
-	if
-}
+	int	i;
+	int	result;
+	int	multiplier;
 
--2147483648
-2147483647
+	i = 0;
+	result = 0;
+	multiplier = 1;
+	while ((nptr[i] >= 9 && nptr[i] <= 13) || nptr[i] == 32)
+		i++;
+	if (nptr[i] == '-' || nptr[i] == '+')
+	{
+		if (nptr[i] == '-')
+			multiplier *= -1;
+		i++;
+	}
+	while (nptr[i] >= '0' && nptr[i] <= '9')
+		result = (nptr[i++] - '0') + (result * 10);
+	return (result * multiplier);
+}
--- teste.c
+++
@@ -1,8 +0,0 @@
-#include <limits.h>
-#include <stdio.h>
-
-int main(void)
-{
-	int a = INT_MIN;
-	printf("%d", a);
-}
