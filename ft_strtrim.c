83e96550b114003ff516ba9241f4eb272563996b
rgomes-d <rgomes-d@student.42sp.org.br>
2025-07-17 21:34:22 -0300

    fix: arrumado a função ft_strtrim e ft_substr

--- ft_strtrim.c
+++ ft_strtrim.c
@@ -5,60 +5,52 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: rgomes-d <rgomes-d@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/07/17 11:49:26 by rgomes-d          #+#    #+#             */
-/*   Updated: 2025/07/17 19:30:50 by rgomes-d         ###   ########.fr       */
+/*   Created: 2025/07/17 19:55:52 by rgomes-d          #+#    #+#             */
+/*   Updated: 2025/07/17 21:32:48 by rgomes-d         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-int	ft_chrdup(const char *str, char c, int begin, int end)
+int	ft_set_value(const char *set, char c)
 {
-	int	count_c;
+	int	i;
 
-	count_c = 0;
-	while (begin < end)
-		if (str[begin++] == c)
-		count_c++;
-	return (count_c == 1);
-}
-
-unsigned int	ft_chrinstr(const char *set, const char *str, int srt, int end)
-{
-	unsigned int	i_set;
-	unsigned int	i_src;
-	int				flag;
-
-	i_src = srt;
-	flag = 0;
-	i_set = 0;
-	while (i_src < (unsigned int)end)
-	{
-		while (set[i_set] != 0)
-		{
-			if (str[i_src] == set[i_set]
-				&& ft_chrdup(str, set[i_set], srt, end))
-			flag++;
-		i_set++;
-		}
-		i_set = 0;
-		i_src++;
-	}
-	return (flag);
+	i = 0;
+	while (set[i] != 0)
+		if (set[i++] == c)
+			return (1);
+	return (0);
 }
 
 char	*ft_strtrim(char const *s1, char const *set)
 {
 	unsigned int	len_s1;
-	unsigned int	len_set;
-	unsigned int	nsrt;
-	unsigned int	nend;
-	char			*dup;
+	unsigned int	rm_begin;
+	unsigned int	rm_end;
 
-	len_set = ft_strlen(set);
-	len_s1 = ft_strlen(s1);
-	nsrt = (ft_chrinstr(set, s1, 0, len_set) == len_set);
-	nend = (ft_chrinstr(set, s1, (len_s1 - len_set), len_s1) == len_set);
-	dup = ft_substr(s1, len_set * nsrt, len_s1 - ((nsrt + nend) * len_set));
-	return (dup);
+	rm_begin = 0;
+	len_s1 = ft_strlen(s1) - 1;
+	rm_end = 0;
+	if(*s1 == 0)
+		return (ft_calloc(1,0));
+	while (ft_set_value(set, s1[rm_begin]))
+		rm_begin++;
+	while (ft_set_value(set, s1[len_s1]))
+	{
+		rm_end++;
+		if (len_s1 <= 0)
+			break ;
+		len_s1--;
+	}
+	if ((rm_begin == rm_end) && (rm_begin == ft_strlen(s1)))
+		return (ft_calloc(1,0));
+	return (ft_substr(s1, rm_begin, (len_s1 - rm_begin) + 1));
 }
+
+// int	main(void)
+// {
+// 	char *s = ft_strtrim("", "123");
+// 	free(s);
+// 	return(0);
+// }
--- ft_substr.c
+++ ft_substr.c
@@ -6,7 +6,7 @@
 /*   By: rgomes-d <rgomes-d@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/16 13:05:01 by rgomes-d          #+#    #+#             */
-/*   Updated: 2025/07/17 11:48:53 by rgomes-d         ###   ########.fr       */
+/*   Updated: 2025/07/17 21:23:03 by rgomes-d         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,8 +20,10 @@ char	*ft_substr(char const *s, unsigned int start, size_t len)
 
 	len_s = ft_strlen(s);
 	i = 0;
-	if (len > (len_s+1)-(start))
-		new_str = ft_calloc(1, len_s + 1);
+	if (len > (len_s + 1)-(start))
+		new_str = ft_calloc(1, (len_s - start) + 1);
+	else if (start > len_s)
+		new_str = ft_strdup("");
 	else
 		new_str = ft_calloc(1, len + 1);
 	while (i < len && s[start + i] != '\0' && (start + i) <= len_s)
@@ -32,3 +34,9 @@ char	*ft_substr(char const *s, unsigned int start, size_t len)
 	new_str[i] = '\0';
 	return (new_str);
 }
+// int	main(void)
+// {
+// 	char *s = ft_substr("0123456789", 9, 10);
+// 	free(s);
+// }
+
