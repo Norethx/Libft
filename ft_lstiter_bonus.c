f02020bab88a27c496f19cf9a6c16b1e53955677
rgomes-d <rgomes-d@student.42sp.org.br>
2025-07-25 12:23:31 -0300

    fix: Funções para a entrega.

--- ft_lstiter_bonus.c
+++ ft_lstiter_bonus.c
@@ -6,7 +6,7 @@
 /*   By: rgomes-d <rgomes-d@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/21 13:32:48 by rgomes-d          #+#    #+#             */
-/*   Updated: 2025/07/21 13:39:56 by rgomes-d         ###   ########.fr       */
+/*   Updated: 2025/07/25 11:36:38 by rgomes-d         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,15 +14,11 @@
 
 void	ft_lstiter(t_list *lst, void (*f)(void *))
 {
-	t_list	*aux;
-
-	aux = lst;
 	if (!lst || !(f))
 		return ;
-	while (aux->next != ((void *)0))
+	while (lst)
 	{
-		(f)(aux->content);
-		aux = aux->next;
+		(f)(lst->content);
+		lst = lst->next;
 	}
-	(f)(aux->content);
 }
